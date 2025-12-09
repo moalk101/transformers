900ddd0c4ae7fc8dda3699cfd65607088898e462
modar <moalk101@hhu.de>
2025-12-09 15:49:35 +0100

    move to src

---
+++ src/modelling/embedding/positional_encoding.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch
+
+
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model, max_len):
+        
+        super().__init__()
+        
+        pe = torch.zeros(max_len,d_model)
+        positions = torch.arange(0,max_len).unsqueeze(1)
+        
+        steps = torch.arange(0,d_model,2)
+        
+        pe[:,0::2] = torch.sin(positions / (10000) ** (steps / d_model))
+        pe[:,1::2] = torch.sin(positions / (10000) ** (steps / d_model))
+
+        pe = pe.unsqueeze(0)
+        
+        self.register_buffer("pe",pe)    
+        
+    def forfward(self,x):
+        
+        length = x.size(1)
+        
+        x = x + self.pe[:,:length,:]    
+        
+        return x
